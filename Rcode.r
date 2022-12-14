47038a46fa285edfb7bfd88ce7ce29f76db7f737
Kyoshiro Hiki <40878629+KyoHiki@users.noreply.github.com>
2022-12-14 14:45:15 +0900

    Update Rcode.r

--- Rcode.r
+++ Rcode.r
@@ -6,14 +6,20 @@ library(readxl)
 library(rsconnect)
 
 
+
 # Define the Shiny app
 shinyApp(
   ui = fluidPage(
+    theme = bslib::bs_theme(bootswatch = "journal"),
     # App title
     titlePanel("Dose-Response Analysis"),
     # Input section
     sidebarLayout(
       sidebarPanel(
+        # Input for selecting the test species
+        radioButtons("Species", "Test species",
+             choices = c( "Algae: TG201","Daphnia: TG202","Fish: TG203" ),
+             selected = "Algae: TG201"),
         # Input for selecting the data file
         fileInput("data_file", "Choose Excel file",
                   accept = c(".xlsx")),
@@ -31,45 +37,58 @@ shinyApp(
       ),
       # Output section
       mainPanel(
-        # Output for displaying the dose-response plot
-        plotOutput("dose_response_plot"),
-        # Output for displaying the EC50 value
-        textOutput("ec50_value")
+          tabsetPanel(
+            tabPanel("Plot",plotOutput("dose_response_plot")),
+            tabPanel("Statistics",textOutput("ec50_value")),
+          ),
+          width=8
+        )
       )
-    )
-  ),
+    ),
+  
   
   # Define the server function
   server = function(input, output) {
     # Reactive function for reading in the data file
-    data <- eventReactive(input$submit, {
+    data_up <- eventReactive(input$submit, {
       # Return NULL if no file is selected
       if (is.null(input$data_file)) {
         return(NULL)
       }
-      # Read in the data file
-      read_excel(input$data_file$datapath)
+      read_excel(input$data_file$datapath) # Read in the data file
     })
-
+ 
     # Reactive function for fitting the dose-response model
     model <- reactive({
-      # Return NULL if no data is available
-      if (is.null(data())) {
-        return(NULL)
-      }
-      # Fit the model
-      drm(as.formula(paste(input$response_var, "~", input$dose_var)),
-          data = data(), fct = LL.4())
-    })
+    # Return NULL if no data is available
+    if (is.null(data_up())) {
+      return(NULL)
+    }
+    # Determine which model to fit based on the selected test species
+      if (input$Species == "Algae: TG201") {
+        # Fit a continuous model for algae
+        
+        Ctr <- data_up() %>% dplyr::filter(input$dose_var == min(input$dose_var)) %>% dplyr::select(input$response_var) %>%
+          summarize(Mean= mean(.)) %>% as.numeric
+        Algae_model <- drm(as.formula(paste(input$response_var, "~", input$dose_var)),data=data_up(),fct = LL.2(upper=Ctr), type=c("continuous"))
+        return(Algae_model)
+        } else {
+        # Fit a binomial model for fish and daphnia
+        Other_model <- drm(as.formula(paste(input$response_var, "~", input$dose_var)),data=data_up(),fct = LL.2(), type=c("binomial") )
+        return(Other_model)
+        }
+      })
+    
     # Reactive function for predicting the fitted values
     fitted_values <- reactive({
       # Return NULL if no model is available
       if (is.null(model())) {
         return(NULL)
       }
-
       # Predict the fitted values
-      predict(model())
+      Conc <- expand.grid(Conc=exp(seq(log(0.001), log(10), length=10000))  )  
+      Pred <- predict(model(), newdata=Conc, interval="confidence") %>% cbind(Conc)
+      return(Pred)
     })
   
     # Reactive function for estimating the EC50 value and its confidence intervals
@@ -79,33 +98,28 @@ shinyApp(
         return(NULL)
       }
       # Estimate the EC50 value and its 95% confidence intervals using the ED function
-      ed_result <- ED(model(), fct = LL.4(), c(50), intervals = "delta")
-      # Extract the EC50 value and its confidence intervals from the result
-      ec50_value <- ed_result[1]
-      ec50_ci_lower <- ed_result[3]
-      ec50_ci_upper <- ed_result[4]
-      # Return the EC50 value and its confidence intervals
-      list(ec50_value = ec50_value,
-           ec50_ci_lower = ec50_ci_lower,
-           ec50_ci_upper = ec50_ci_upper)
+      ed_result <- ED(model(), c(50), intervals = "delta")
+      return(data.frame(EC50=ed_result[1],Lower=ed_result[3],Upper=ed_result[4]))
     })
 
     # Reactive function for plotting the dose-response curve
     output$dose_response_plot <- renderPlot({
       # Return if no data is available
-      if (is.null(data())) {
+      if (is.null(data_up())) {
         return()
       }
-      # Plot the data and the fitted values
-      ggplot(data(), aes_string(x = input$dose_var, y = input$response_var)) +
-        geom_point() +
-        geom_line(aes(y = fitted_values()))+
+      # Plot the data and the fitted line
+      ggplot() +
+        geom_point(data=data_up(), aes_string(x = input$dose_var, y = input$response_var), size=4,pch=19) +
+        geom_line(data=fitted_values(), aes(x=Conc, y =Prediction))+
         scale_x_log10()+
         labs(x=paste("Concentration (",input$unit_var,")",sep=""))+
         coord_cartesian(clip = "off")+ 
         annotation_logticks(sides = "b", size = 1, outside = TRUE) +
         theme_bw(base_size=25)
-    })
+    }, height = 500, width = 600
+    )
+    
     # Reactive function for displaying the EC50 value
     output$ec50_value <- renderText({
      # Return if no model is available
@@ -113,7 +127,8 @@ shinyApp(
        return("")
      }
      # Return the EC50 value
-     paste("EC50 value: ", round(ec50_ci()$ec50_value, 3)," (95% CI: ",round(ec50_ci()$ec50_ci_lower, 3),"-",round(ec50_ci()$ec50_ci_upper, 3),")")
+     paste("EC50 value: ", round(ec50_ci()$EC50, 3)," (95% CI: ",round(ec50_ci()$Lower, 3),"-",round(ec50_ci()$Upper, 3),")")
   })
   }
 )
+
